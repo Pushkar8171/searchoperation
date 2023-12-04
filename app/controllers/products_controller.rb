a9eac195cd441bbb17ec565ffb02c410312aaa78
Pushkar8171 <PushkarSinghRathore8171@gmail.com>
2023-12-04 16:31:16 +0530

    Added ransack to the Products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,13 @@ class ProductsController < ApplicationController
 
   def index 
     @products = Product.paginate(page: params[:page], per_page: 5)
+    
+
 	end 
+
+  def search 
+    @q = Product.ransack(params[:q])
+    @product = @q.result(distinct: true)
+
+  end 
 end
