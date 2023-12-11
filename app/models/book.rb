7f01e6222ee3825c8d0767d67b87d7fee7b1d12c
Pushkar8171 <PushkarSinghRathore8171@gmail.com>
2023-12-11 13:28:48 +0530

    Added Filter Functionality in the Books

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,11 @@
 class Book < ApplicationRecord
     ransack
   def self.ransackable_attributes(auth_object = nil)
-    %w[name] # List the attributes you want to make searchable
+    %w[name  genre] # List the attributes you want to make searchable
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
   end
 
   self.per_page = 20
